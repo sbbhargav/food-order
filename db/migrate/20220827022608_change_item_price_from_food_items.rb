bd9a5595148fa2a593358569e567b0a57c68a08d
sbbhargav <sb.bhargav1993@gmail.com>
2022-08-30 16:19:15 +0530

    fifth commit

--- db/migrate/20220827022608_change_item_price_from_food_items.rb
+++ db/migrate/20220827022608_change_item_price_from_food_items.rb
@@ -1,5 +1,9 @@
 class ChangeItemPriceFromFoodItems < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :food_items, :item_price, :integer
+  end
+
+  def down
+    change_column :food_items, :item_price, :string
   end
 end
